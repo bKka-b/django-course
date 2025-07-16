5c23421b67ee1c4f40abb689af6be7b1cedae181
bKka_b <becky.polak@gmail.com>
2025-07-16 08:59:25 -0400

    Dockerizing (localhost issues)
    Dockerized the project, but had to hard code in IP instead of using .env file

--- practiceSite/myproject/myproject/settings.py
+++ practiceSite/myproject/myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9o6j#lzs2*ivbive0lf$48%k&soi66bh)erdz+8w_689n&*56l'
+#   SECRET_KEY = 'django-insecure-9o6j#lzs2*ivbive0lf$48%k&soi66bh)erdz+8w_689n&*56l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#   DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+# The secret key - Added for Docker
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
+#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -71,14 +76,20 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
+# OLD https://docs.djangoproject.com/en/dev/ref/settings/#databases
+# Current for Docker
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'polls'),
+         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
 
 
 # Password validation
